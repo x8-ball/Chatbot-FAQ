6629a274529ae1562484919d45e5619ecc6a6fe9
8-Ball <thomes.f@hotmail.de>
2018-05-15 11:45:15 +0200

    Brain update

--- modell/trainAndPredict.py
+++ modell/trainAndPredict.py
@@ -3,7 +3,7 @@ from rasa_nlu.config import RasaNLUConfig
 from rasa_nlu.model import Trainer
 from rasa_nlu.model import Metadata, Interpreter
 config = RasaNLUConfig("config_chatbot_spacy.json")
-training_data = load_data('data/faq_lab1.json')
+training_data = load_data('data/commands.json')
 trainer = Trainer(config)
 trainer.train(training_data)
 # Returns the directory the model is stored in
@@ -12,11 +12,13 @@ model_directory = trainer.persist('./projects/')
 # where `model_directory points to the folder the model is persisted in
 interpreter = Interpreter.load(model_directory, config)
 
-q1 = u"Wann ist das Labor?"
-q2 = u"wie funktioniert wireshark?"
+q1 = u"was ist arp?"
+q2 = u"was ist ping?"
+q3 = u"was ist ping"
+
 a1 = interpreter.parse(q1)
 a2 = interpreter.parse(q2)
-
+a3 = interpreter.parse(q3)
 print("\n\n#################################")
 print(str(model_directory))
 print("\n\n#################################")
@@ -29,3 +31,8 @@ print(q2)
 obj2 = a2['intent_ranking'][0]
 print("intent :" + str(obj2['name']) + " with " + str(obj2['confidence']))
 print(a2)
+
+print(q3)
+obj3 = a3['intent_ranking'][0]
+print("intent :" + str(obj3['name']) + " with " + str(obj3['confidence']))
+print(a3)
