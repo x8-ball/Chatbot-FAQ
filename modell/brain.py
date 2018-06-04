c840ca9cf52b24c6b755ebc0ce6c4cbfef0719f5
8-Ball <thomes.f@hotmail.de>
2018-06-04 17:41:12 +0200

    brain in modell Ordner verschoben

---
+++ modell/brain.py
@@ -0,0 +1,22 @@
+import aiml
+import os
+import sys
+from inspect import getsourcefile
+import os.path
+
+kernel = aiml.Kernel()
+
+
+current_path = os.path.dirname(os.path.abspath(getsourcefile(lambda:0)))
+print current_path
+aimlPath = current_path+'/aiml/'
+
+if os.path.isfile(aimlPath+"bot_brain.brn"):
+    kernel.bootstrap(brainFile = aimlPath+ "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = aimlPath + "std-startup.xml", commands = "load aiml b")
+    kernel.saveBrain(aimlPath+"bot_brain.brn")
+
+# kernel now ready for use
+def calcResponse(question):
+	return kernel.respond(question)
