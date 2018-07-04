d81eb3d1817fcae37e5c7be9210bcaf2685d3b5f
8-Ball <thomes.f@hotmail.de>
2018-07-04 16:05:30 +0200

    Brain File is now created every time

--- modell/brain.py
+++ modell/brain.py
@@ -10,13 +10,17 @@ kernel = aiml.Kernel()
 current_path = os.path.dirname(os.path.abspath(getsourcefile(lambda:0)))
 print current_path
 aimlPath = current_path+'/aiml/'
-
+"""
 if os.path.isfile(aimlPath+"bot_brain.brn"):
     kernel.bootstrap(brainFile = aimlPath+ "bot_brain.brn")
 else:
-    kernel.bootstrap(learnFiles = aimlPath + "std-startup.xml", commands = "load aiml b")
+"""
+kernel.bootstrap(learnFiles = aimlPath + "std-startup.xml", commands = "load aiml b")
+"""
     kernel.saveBrain(aimlPath+"bot_brain.brn")
-
+"""
 # kernel now ready for use
 def calcResponse(question):
 	return kernel.respond(question)
+
+calcResponse('hi')
