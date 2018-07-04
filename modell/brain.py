78b4009da6e650f2ac8a26bd994f94ab4fb002ca
8-Ball <thomes.f@hotmail.de>
2018-07-04 16:25:36 +0200

    Fixxed Links to Alice

--- modell/brain.py
+++ modell/brain.py
@@ -10,17 +10,14 @@ kernel = aiml.Kernel()
 current_path = os.path.dirname(os.path.abspath(getsourcefile(lambda:0)))
 print current_path
 aimlPath = current_path+'/aiml/'
-"""
+
 if os.path.isfile(aimlPath+"bot_brain.brn"):
     kernel.bootstrap(brainFile = aimlPath+ "bot_brain.brn")
 else:
-"""
-kernel.bootstrap(learnFiles = aimlPath + "std-startup.xml", commands = "load aiml b")
-"""
-    kernel.saveBrain(aimlPath+"bot_brain.brn")
-"""
+	kernel.bootstrap(learnFiles = aimlPath + "std-startup.xml", commands = "LOAD AIML B")
+	kernel.saveBrain(aimlPath+"bot_brain.brn")
+
+print "aimlPath: " + aimlPath + "std-startup.xml"
 # kernel now ready for use
 def calcResponse(question):
 	return kernel.respond(question)
-
-calcResponse('hi')
