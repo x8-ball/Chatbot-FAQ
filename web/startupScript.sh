445c020ee8f6446a2d118a98daac423082c710de
8-Ball <thomes.f@hotmail.de>
2018-07-17 16:57:50 +0200

    requirements und startupScript für RPI verschoben

--- web/startupScript.sh
+++ web/startupScript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean;
-sudo apt-get install -y python-pip python-flask python-flask-sockets;
-#sudo pip --no-cache-dir install -r requirementsServer.txt;
-sudo pip install flask, flask-socketio python-aiml;
+#sudo apt-get install -y python-pip python-flask python-flask-sockets;
+#sudo pip install flask, flask-socketio python-aiml;
+sudo pip --no-cache-dir install -r requirements.txt;
 cd web;
 export FLASK_APP=webserver.py;
 python -m flask run --host=0.0.0.0;
