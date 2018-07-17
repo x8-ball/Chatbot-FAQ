506e48f4f75405e56bf3083950f962770e8caf9b
8-Ball <thomes.f@hotmail.de>
2018-07-17 20:28:07 +0200

    Webpath added

--- web/installScript.sh
+++ web/installScript.sh
@@ -2,6 +2,7 @@
 #sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean;
 #sudo apt-get install -y python-pip python-flask python-flask-sockets;
 #sudo pip install flask, flask-socketio python-aiml;
+cd ~/Chatbot-FAQ/web
 sudo pip --no-cache-dir install -r requirements.txt;
 
 LINE='bash ~/Chatbot-FAQ/web/startupScript.sh'
