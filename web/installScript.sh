4618cb3da39f4e11bdd5791d7d6e375db80ab72f
8-Ball <thomes.f@hotmail.de>
2018-07-17 17:34:50 +0200

    Installations und Startup Scropt in Web gelegt

--- web/installScript.sh
+++ web/installScript.sh
@@ -2,5 +2,6 @@
 #sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean;
 #sudo apt-get install -y python-pip python-flask python-flask-sockets;
 #sudo pip install flask, flask-socketio python-aiml;
-export FLASK_APP=webserver.py;
-python -m flask run --host=0.0.0.0;
+sudo pip --no-cache-dir install -r requirements.txt;
+line="@reboot ~/Chatbot-FAQ/web/startupScript.sh"
+(crontab -u pi -l; echo "$line" ) | crontab -u pi -
