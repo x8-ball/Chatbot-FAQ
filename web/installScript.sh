c37534daae4bb0f5a469dedd33d09148d800542d
8-Ball <thomes.f@hotmail.de>
2018-07-18 15:23:49 +0200

    installscript: final version die erste

--- web/installScript.sh
+++ web/installScript.sh
@@ -3,11 +3,10 @@
 #sudo apt-get install -y python-pip python-flask python-flask-sockets;
 #sudo pip install flask, flask-socketio python-aiml;
 cd ~/Chatbot-FAQ/web
-sudo apt-get -y install python-pip
-sudo pip --no-cache-dir install -r requirements.txt;
+sudo apt-get -y install python-pip libevent-dev python-all-dev;
+sudo pip install -r requirements.txt;
 
 LINE='bash ~/Chatbot-FAQ/web/startupScript.sh'
 FILE=~/.bashrc
-crontab -l | grep -q 'search string'  && echo 'crontab wurde wieder gesetzt' || grep -qF "$LINE" "$FILE"  || echo "$LINE" | sudo tee --append "$FILE"
-#crontab -l | grep -q 'search string'  && echo 'entry exists' || echo 'entry does not exist'
-#reboot
+crontab -l | grep -q 'search string'  && echo 'crontab schon vorhanden' || grep -qF "$LINE" "$FILE"  || echo "$LINE" | sudo tee --append "$FILE"
+reboot
