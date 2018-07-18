80a7e0268b4ead24f8b68d7a948cb5c09bb25a25
8-Ball <thomes.f@hotmail.de>
2018-07-18 15:10:21 +0200

    requirements angepasst

--- web/installScript.sh
+++ web/installScript.sh
@@ -3,11 +3,11 @@
 #sudo apt-get install -y python-pip python-flask python-flask-sockets;
 #sudo pip install flask, flask-socketio python-aiml;
 cd ~/Chatbot-FAQ/web
-sudo apt-get install pip
+sudo apt-get -y install python-pip
 sudo pip --no-cache-dir install -r requirements.txt;
 
 LINE='bash ~/Chatbot-FAQ/web/startupScript.sh'
 FILE=~/.bashrc
 crontab -l | grep -q 'search string'  && echo 'crontab wurde wieder gesetzt' || grep -qF "$LINE" "$FILE"  || echo "$LINE" | sudo tee --append "$FILE"
 #crontab -l | grep -q 'search string'  && echo 'entry exists' || echo 'entry does not exist'
-reboot
+#reboot
