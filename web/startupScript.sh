d14a5e96e65e3627666f95267790803d53fe2516
8-Ball <thomes.f@hotmail.de>
2018-07-20 15:43:01 +0200

    git pull kommentare

--- web/startupScript.sh
+++ web/startupScript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Updatet den Raspberry Pi
 #sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean;
-#sudo apt-get install -y python-pip python-flask python-flask-sockets;
-#sudo pip install flask, flask-socketio python-aiml;
+#Wechsel in Chatbot-Verzeichnis und zieht neueste Dateien von Repository, insofern 
+#cd ~/Chatbot-FAQ
+#git pull
 cd ~/Chatbot-FAQ/web
 export FLASK_APP=webserver.py;
 python -m flask run --host=0.0.0.0;
