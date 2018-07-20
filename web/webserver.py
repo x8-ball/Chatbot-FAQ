81adf2ee2aa14b2b96a38855fa9cc1aa042f0acd
8-Ball <thomes.f@hotmail.de>
2018-07-20 15:42:49 +0200

    Logik an Brain angepasst

--- web/webserver.py
+++ web/webserver.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask,render_template,request
 from flask_socketio import SocketIO, send
 
 import sys
@@ -12,12 +14,22 @@ socketio = SocketIO(app)
 
 @socketio.on('message')
 def handleMessage(msg):
+	#print request.remote_addr
 	if(msg['type']):
 		if(msg['type'] == "question"):
 			print('User fragte: ' + msg['text'])
-			answer = brain.calcResponse(msg['text'])
+			answer = brain.calcResponse(msg['text'],request.remote_addr)
 			send({"answer" : answer})
 			print('Antwort: ' + str(answer))
+
+@socketio.on('connect')
+def on_connect():
+    if(not brain.loadBrain(request.remote_addr)):
+    	send({"answer" : "Robi ist gerade sehr beschäftigt und hat keine Zeit"})
+
+@socketio.on('disconnect')
+def on_disconnect():
+	brain.deleteBrain(request.remote_addr)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
